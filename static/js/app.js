9ccfd7151b5d094e0b79ea88532b4b0669b6d3a6
midzer <midzer@gmail.com>
2020-03-29 22:45:44 +0200

    load quicklink only if Intersection Observer available

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,13 @@
-quicklink({
-  ignores: [
-      /.jpg/,
-      /.pdf/
-  ]
-});
+if ('IntersectionObserver' in window) {
+  const script = document.createElement('script');
+  script.src = '/js/quicklink.js';
+  script.onload = function() {
+    quicklink({
+      ignores: [
+          /.jpg/,
+          /.pdf/
+      ]
+    });
+  };
+  document.head.appendChild(script);
+}
